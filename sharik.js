865fb206c7908c52c993c4b6013105bb66c60be6
khaled <khaledrefaat172@gmail.com>
2026-04-25 04:40:08 +0300

    Add crash logging

--- sharik.js
+++ sharik.js
@@ -1,4 +1,12 @@
 const express = require("express");
+
+process.on('uncaughtException', (err) => {
+    console.error('🚨 UNCAUGHT EXCEPTION:', err);
+});
+process.on('unhandledRejection', (reason, promise) => {
+    console.error('🚨 UNHANDLED REJECTION:', reason);
+});
+
 const app = express();
 const port = process.env.PORT || 3000;
 const mongoose = require("mongoose");
